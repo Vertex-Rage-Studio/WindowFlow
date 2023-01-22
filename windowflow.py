97547aa1f17da0b5b5900fbb88037bff06765511
BartoszBroda <bartosz.broda@gmail.com>
2023-01-22 18:19:12 +0100

    First test of how to open and move windows

--- windowflow.py
+++ windowflow.py
@@ -1,7 +1,37 @@
+import sys
+import os
+import subprocess
+import win32gui
+import win32api
+import win32con
+import time
 
 
+def get_set_of_window_titles(hwnd, output):
+    if win32gui.IsWindowVisible(hwnd):
+        title = win32gui.GetWindowText(hwnd)
+        output.add(title)
+    
 def main():
-    print("Hellow world!")
+    #config_file = sys.argv[1]
+
+    app_name = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+    app_params = 'https://google.com'
+
+    start_windows = set([])
+
+    win32gui.EnumWindows(get_set_of_window_titles, start_windows)
+
+    print(start_windows)
+
+    print("Launching...", app_name)
+
+    subprocess.Popen([app_name, app_params])
+    time.sleep(0.5) # that's stupid, but it works for now
+    active = win32gui.GetForegroundWindow()
+    print(win32gui.GetWindowText(active))
+    win32gui.SetWindowPos(active, None, 10, 10, 1000, 500, win32con.SWP_SHOWWINDOW)
+
 
 if __name__ == "__main__":
     main()
