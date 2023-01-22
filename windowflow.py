27b27dca35720eb17e7523d523fb8c0dcf98c33c
BartoszBroda <bartosz.broda@gmail.com>
2023-01-22 22:21:39 +0100

    Base workflow done (without error handling)

--- windowflow.py
+++ windowflow.py
@@ -8,14 +8,16 @@ import time
 
 def parse_config(filename):
     '''
-    Not handling any errors for now.
+    Parses the config file.
+
+    It doesn't handle any errors for now.
     Line format:
-    #appname|x,y,width,height|arguments|delay till app becomes responsive(sec)
+    #appname|x,y,width,height|delay till app becomes responsive(sec)|arg1|arg2|argn
     '''
 
     parsed_config = []
     for line in open(filename):
-        print(line)
+        
         line = line.strip()
         if line[0] == '#' or len(line) == 0: 
             continue
@@ -23,46 +25,55 @@ def parse_config(filename):
 
         appname = params[0]
         x,y, width, height = params[1].split(',')
-        arguments = params[2]
-        delay = float(params[3])
+        x = int(x)
+        y = int(y)
+        width = int(width)
+        height = int(height)                
+        delay = float(params[2])
+        arguments = params[3:]
 
-        parsed_config.append( (appname, (x,y,width,height), arguments, delay ) )
+        parsed_config.append( (appname, (x,y,width,height), delay, arguments ) )
 
     return parsed_config
 
 def get_set_of_window_titles(hwnd, output):
+    '''
+    Not used yet, gets all windows titles
+    '''
     if win32gui.IsWindowVisible(hwnd):
         title = win32gui.GetWindowText(hwnd)
         output.add(title)
-    
-def main():
-    #config_file = sys.argv[1]
-    config_file = 'config_test.cfg'
 
-    app_name = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
-    app_params = '-new-window https://google.com'
-
-    start_windows = set([])
-
-    win32gui.EnumWindows(get_set_of_window_titles, start_windows)
-
-    print(start_windows)
-
-    print("Launching...", app_name)
-
-    subprocess.Popen([app_name, app_params])
-    time.sleep(0.5) # that's stupid, but it works for now
-    active = win32gui.GetForegroundWindow()
-    print(win32gui.GetWindowText(active))
-    win32gui.SetWindowPos(active, None, -1920, 0, 1000, 1030, win32con.SWP_SHOWWINDOW)
-
-    config_parsed = parse_config(config_file)
+def restore_session(config_parsed):
+    '''
+    Restores windows layout based on current config file
+    '''
     for c in config_parsed:
         app_name = c[0]
-        position = c[1]
-        args = c[2]
-        delay = c[3]
-        print(app_name, position, args, delay)
+        x,y,w,h = c[1]
+        delay = c[2]
+        args = c[3]
+        
+        params = [app_name]
+        params.extend(args)
+
+        subprocess.Popen(params)
+        time.sleep(delay) # that's stupid, but it works for now
+        active = win32gui.GetForegroundWindow()
+        win32gui.SetWindowPos(active, None, x,y,w,h, win32con.SWP_SHOWWINDOW)
+    
+def main():
+    config_file = sys.argv[1]
+
+    # start_windows = set([])
+    # win32gui.EnumWindows(get_set_of_window_titles, start_windows)
+    # print(start_windows)
+
+    config_parsed = parse_config(config_file)
+
+    restore_session(config_parsed)
+
+
 
 
 if __name__ == "__main__":
