1699a4523da5e73dc5f18a16224a675bb0bf2832
Matthew Zenn <spyguy57@gmail.com>
2023-01-23 11:06:59 -0500

    Added tool script

--- WinFlow.py
+++ WinFlow.py
@@ -1,5 +1,10 @@
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import os
+import subprocess
+
+path = "./Configs"
+dir_list = os.listdir(path)
 
 app = QApplication([])
 app.setQuitOnLastWindowClosed(False)
@@ -14,16 +19,33 @@ tray.setVisible(True)
 
 # Creating the options
 menu = QMenu()
-option1 = QAction("Load")
 option2 = QAction("Save")
-menu.addAction(option1)
+
+for i in dir_list:
+    count = 1
+    string = str(count)
+    (string) = QAction(i)
+    menu.addAction(i)
+    count += 1
+
+menu.addSeparator()
 menu.addAction(option2)
 
+# Runs Load Script
+file = QAction()
+file.triggered.connect(
+    subprocess.run('python windowflow.py -c' + file.text())
+)
 # To quit the app
 quit = QAction("Quit")
 quit.triggered.connect(app.quit)
 menu.addAction(quit)
 
+menu.setStyleSheet(
+    "background-color: rgb(20, 20, 20);"
+    "border: 1px solid rgb(40, 40, 40);"
+    "color: rgb(150, 150, 150);"
+)
 # Adding options to the System Tray
 tray.setContextMenu(menu)
 
